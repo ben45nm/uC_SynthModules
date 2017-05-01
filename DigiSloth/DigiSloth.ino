f1835d3aa3025b2cf31c78a3442d3332c555efb0
ben@45nm <ben@45nm.net>
2017-05-01 13:08:44 +0200

    rewrite digisloth

--- DigiSloth/DigiSloth.ino
+++ DigiSloth/DigiSloth.ino
@@ -2,105 +2,89 @@
  *   Mozzis HIFI-Mode must be enabled for the PWM trick.
  *  Outputs three independent, randomly changing voltages.
  *  
- *  Minimum time range 1s - 3s
- *  Maximum time range 10s - 30s
- *  
  *  Using Mozzi sonification library by Tim Barrass: http://sensorium.github.com/Mozzi/
  *  
  *  ben @ 45nm, 2017.
 */
 
 #include <MozziGuts.h>
-#include <Line.h>
-#include <EventDelay.h>
+#include <Oscil.h> //
+#include <tables/sin2048_int8.h>
 #include <mozzi_rand.h>
 
-Line <long> lineA, lineB, lineC;
-EventDelay edA, edB, edC;
-int minA, maxA, minB, maxB, minC, maxC;
-#define CONTROL_RATE 1024
-
-// LED pins, use 0-7 for PORTD, otherwise change to PORTB in updatePins
 const byte A_PIN = 3;
 const byte B_PIN = 4;
 const byte C_PIN = 5;
 
+byte a, b, c;
+float freqA, freqB, freqC;
+
+Oscil <SIN2048_NUM_CELLS, CONTROL_RATE> sinA(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, CONTROL_RATE> sinB(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, CONTROL_RATE> sinC(SIN2048_DATA);
+
+void updatePins(byte a, byte b, byte c) {
+  static byte a_count = 0;
+  static byte b_count = 85;
+  static byte c_count = 170;
+  (a_count++ >= a) ? PORTD &= ~(1 << A_PIN) : PORTD |= (1 << A_PIN);
+  (b_count++ >= b) ? PORTD &= ~(1 << B_PIN) : PORTD |= (1 << B_PIN);
+  (c_count++ >= c) ? PORTD &= ~(1 << C_PIN) : PORTD |= (1 << C_PIN);
+}
+
 void setup() {
-  startMozzi(CONTROL_RATE);
-  // on startup set default values to work with
-  minA, maxA, minB, maxB, minC, maxC = 0;
-  pinMode(A_PIN,OUTPUT);
-  pinMode(B_PIN,OUTPUT);
-  pinMode(C_PIN,OUTPUT);
+  pinMode(A_PIN, OUTPUT);
+  pinMode(B_PIN, OUTPUT);
+  pinMode(C_PIN, OUTPUT);
+  sinA.setFreq(0.01f);
+  sinB.setFreq(0.006f);
+  sinC.setFreq(0.008f);
+  startMozzi(1024);
+}
+
+void updateControl() {
+  
+  int timeValue = map(mozziAnalogRead(A1), 0, 1023, 3000, 30000);
+  
+  if (sinA.next() == 0 || sinA.next() == 100 || sinA.next()  == -100) {
+    int minTime = (int) timeValue * 0.5;
+    int maxTime = (int) timeValue * 1.5;
+    int timeA = rand(minTime, maxTime);
+    int x = rand(0, 50);
+    freqA = (float) x / timeA;
+    sinA.setFreq(freqA);
+  }
+
+  if (sinB.next() == 0 || sinB.next() == 100 || sinB.next() == -100) {
+    int minTime = (int) timeValue * 0.5;
+    int maxTime = (int) timeValue * 1.5;
+    int timeA = rand(minTime, maxTime);
+    int x = rand(0, 60);
+    freqB = (float) x / timeA;
+    sinB.setFreq(freqB);
+  }
+
+  if (sinC.next() == 0 || sinC.next() == 100) {
+    int minTime = (int) timeValue * 0.5;
+    int maxTime = (int) timeValue * 1.5;
+    int timeA = rand(minTime, maxTime);
+    int x = rand(0, 45);
+    freqC = (float) x / timeA;
+    sinC.setFreq(freqC);
+  }
+
+  a = 128 + sinA.next();
+  b = 128 + sinB.next();
+  c = 128 + sinC.next();
+
+}
+
+int updateAudio() {
+  updatePins(a, b, c);
+  return 0;
 }
 
 void loop() {
   audioHook();
 }
 
-void updateControl() {
-
-  // read pot value and map it to milliseconds/seconds (2s -20s)
-  int timeValue = map(mozziAnalogRead(A1), 0, 1023, 2000, 20000);
-
-  // will run/be ready when the previous line has ended
-  if (edA.ready()) {
-    minA = maxA; // set new min to the previous max value
-    maxA = rand(0, 255); // set new max value
-
-    // calculate a range from pot value to calculate pseudo random line timing
-    int minTime = (int) timeValue * 0.5;
-    int maxTime = (int) timeValue * 1.5;
-    int timeA = rand(minTime, maxTime);
-    
-    edA.set(timeA); // set length of next line to event delay timing
-    lineA.set(minA, maxA, ms2steps(timeA)); // set line values (start value, end value, steps)
-    edA.start(); // start event delay
-  }
-
-  if (edB.ready()) {
-    minB = maxB;
-    maxB = rand(0, 255);
-    int minTime = (int) timeValue * 0.5;
-    int maxTime = (int) timeValue * 1.5;
-    int timeB = rand(minTime, maxTime);
-    edB.set(timeB);
-    lineB.set(minB, maxB, ms2steps(timeB));
-    edB.start();
-  }
-
-  if (edC.ready()) {
-    minC = maxC;
-    maxC = rand(0, 255);
-    int minTime = (int) timeValue * 0.5;
-    int maxTime = (int) timeValue * 1.5;
-    int timeC = rand(minTime, maxTime);
-    edC.set(timeC);
-    lineC.set(minC, maxC, ms2steps(timeC));
-    edC.start();
-  }
-
-  // update pins with actual values from the lines
-  updatePins(lineA.next(), lineB.next(), lineC.next());
-
-}
-
-// convert milliseconds to steps @ control rate
-int ms2steps(int ms) {
-  return (int) (ms / 1000) * CONTROL_RATE;
-}
-
-void updatePins(byte a, byte b, byte c){
-  // stagger pwm counter starts to try to reduce combined flicker
-  static byte a_count=0;
-  static byte b_count=85;
-  static byte c_count=170;
-  // PORTD maps to Arduino digital pins 0 to 7
-  (a_count++ >= a) ? PORTD &= ~(1 << A_PIN) : PORTD |= (1 << A_PIN);
-  (b_count++ >= b) ? PORTD &= ~(1 << B_PIN) : PORTD |= (1 << B_PIN);
-  (c_count++ >= c) ? PORTD &= ~(1 << C_PIN) : PORTD |= (1 << C_PIN);
-}
-
-int updateAudio() {
-  return 0; // do nothing
-}
